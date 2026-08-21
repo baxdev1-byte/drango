be0b00ad7a8af2fe973d016c0c63f4e7e377296b
MOHSEN <baxdev1@gmail.com>
2026-08-21 16:43:10 +0330

    add static/media root and url

--- Drango/settings.py
+++ Drango/settings.py
@@ -115,3 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
