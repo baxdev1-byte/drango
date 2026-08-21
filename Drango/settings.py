e93790fe9327e34429cba8b4eefe0ec43a045a42
MOHSEN <baxdev1@gmail.com>
2026-08-21 20:46:33 +0330

    add a new app named website

--- Drango/settings.py
+++ Drango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Installed Apps
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +123,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'statics',
 ]
